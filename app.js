6c09c98b1554f559cc9ba326c54e40b00d6b9992
Melvin Carvalho <melvincarvalho@gmail.com>
2023-05-21 17:11:28 +0200

    blue colour and cleanup

--- app.js
+++
@@ -1,4 +0,0 @@
-import { html, render } from './js/standalone.module.js'
-import Sidebar from './index.js'
-
-render(html`<${Sidebar} />`, document.body)
---
+++ js/app.js
@@ -0,0 +1,4 @@
+import { html, render } from '../js/standalone.module.js'
+import Sidebar from '../lib/index.js'
+
+render(html`<${Sidebar} />`, document.body)
